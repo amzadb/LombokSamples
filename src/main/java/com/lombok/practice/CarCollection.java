e94aa367794576384d0b6c51a13a6775ec26eb4c
Amzadbasha Shaik <amzads@ispace.com>
2021-07-08 15:09:06 +0530

    Updated CarCollection and code refactored CarMapDemo

--- src/main/java/com/lombok/practice/CarCollection.java
+++ src/main/java/com/lombok/practice/CarCollection.java
@@ -32,6 +32,7 @@ public class CarCollection {
 		cars.add(new Car("Maruti Suzuki", "Celerio", Color.Blue));
 		cars.add(new Car("Hyundai", "i20", Color.Silver));
 		cars.add(new Car("MG", "Gloster", Color.Red));
+		cars.add(new Car("Mahindra", "Thar", Color.White));
 		cars.add(new Car("Maruti Suzuki", "Alto", Color.Red));
 		cars.add(new Car("Honda", "City", Color.Grey));
 		cars.add(new Car("Nissan", "Kicks", Color.Black));
@@ -39,23 +40,32 @@ public class CarCollection {
 		cars.add(new Car("Hyundai", "i10", Color.Black));
 		cars.add(new Car("Renault", "Duster", Color.Green));
 		cars.add(new Car("Honda", "CR-V", Color.Black));
+		cars.add(new Car("Audi", "A6", Color.Black));
 		cars.add(new Car("Hyundai", "Santro", Color.Grey));
 		cars.add(new Car("Honda", "WR-V", Color.Red));
 		cars.add(new Car("Mahindra", "Alturas", Color.Blue));
 		cars.add(new Car("Kia", "Carnival", Color.White));
 		cars.add(new Car("Renault", "Kwid", Color.Red));
 		cars.add(new Car("Maruti Suzuki", "Ertiga", Color.Grey));
+		cars.add(new Car("Audi", "Q8", Color.White));
+		cars.add(new Car("Jeep", "Compass", Color.Black));
+		cars.add(new Car("Hyundai", "Verna", Color.Blue));
+		cars.add(new Car("Mahindra", "KUV-100", Color.Grey));
 		cars.add(new Car("Maruti Suzuki", "Swift DZire", Color.White));
 		cars.add(new Car("Skoda", "Octavia", Color.Red));
 		cars.add(new Car("Honda", "Jazz", Color.Yellow));
 		cars.add(new Car("Renault", "Triber", Color.Blue));
+		cars.add(new Car("Jeep", "Wrangler", Color.Yellow));
+		cars.add(new Car("Hyundai", "Elantra", Color.Grey));
 		cars.add(new Car("Maruti Suzuki", "Vitara Brezza", Color.White));
 		cars.add(new Car("Skoda", "Rapid", Color.Blue));
 		cars.add(new Car("Tata", "Indica", Color.Silver));
 		cars.add(new Car("Maruti Suzuki", "Vitara Brezza", Color.White)); // duplicate
 		cars.add(new Car("Skoda", "Superb", Color.White));
+		cars.add(new Car("Mahindra", "TUV-300", Color.Black));
 		cars.add(new Car("Ford", "EcoSport", Color.White)); // duplicate
 		cars.add(new Car("Tata", "Nano", Color.Yellow));
+		cars.add(new Car("Mahindra", "Bolero", Color.Silver));
 		
 		return cars;
 	}
--- src/main/java/com/lombok/practice/CarMapDemo.java
+++ src/main/java/com/lombok/practice/CarMapDemo.java
@@ -9,24 +9,31 @@ public class CarMapDemo {
 
 	public static void main(String[] args) {
 		Collection<Car> cars = CarCollection.getCars();
-		
 		System.out.println("Total Cars: " + cars.size());
 		System.out.println();
 
+		showCarsByColor(cars);
+		System.out.println("---------------------------------------");
+		System.out.println();
+		showCarsByBrand(cars);
+	}
+
+	private static void showCarsByBrand(Collection<Car> cars) {
+		Map<String, List<Car>> carsMapByBrand = cars.stream().collect(Collectors.groupingBy(Car::getBrand));
+		System.out.println("Cars Map by Brand: " + carsMapByBrand.size());
+		carsMapByBrand.forEach((key, list) -> {
+			System.out.println("List of " + key + " cars: " + list.size());
+			list.forEach(car -> System.out.println(car));
+			System.out.println();
+		});
+	}
+
+	private static void showCarsByColor(Collection<Car> cars) {
 		Map<Color, List<Car>> carsMapByColor = cars.stream().collect(Collectors.groupingBy(Car::getColor));
 		System.out.println("Cars Map by Color: " + carsMapByColor.size());
 		carsMapByColor.forEach((k, v) -> System.out.println("Key: " + k + ", Value: " + v));
 		System.out.println();
 		carsMapByColor.forEach((key, list) -> {
-			System.out.println("List of " + key + " cars: " + list.size());
-			list.forEach(car -> System.out.println(car));
-			System.out.println();
-		});
-		
-		
-		Map<String, List<Car>> carsMapByBrand = cars.stream().collect(Collectors.groupingBy(Car::getBrand));
-		System.out.println("Cars Map by Brand: " + carsMapByBrand.size());
-		carsMapByBrand.forEach((key, list) -> {
 			System.out.println("List of " + key + " cars: " + list.size());
 			list.forEach(car -> System.out.println(car));
 			System.out.println();
